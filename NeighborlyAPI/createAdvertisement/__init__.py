ed1df80d457b4cc994c3e2c71be5065b4bb436fa
ductoan19 <ductoan.dsn@gmail.com>
2023-10-10 21:10:29 +0700

    first commit

---
+++ NeighborlyAPI/createAdvertisement/__init__.py
@@ -0,0 +1,29 @@
+import azure.functions as func
+import pymongo
+import json
+import os
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    request = req.get_json()
+
+    if request:
+        try:
+            url = os.environ["MONGO_DB"]
+            client = pymongo.MongoClient(url)
+            database = client['azure']
+            collection = database['advertisements']
+            collection.insert_one(json.loads(request))
+
+            return func.HttpResponse(req.get_body())
+
+        except ValueError:
+            print("could not connect to mongodb")
+            return func.HttpResponse('Could not connect to mongodb', status_code=500)
+
+    else:
+        return func.HttpResponse(
+            "Please pass name in the body",
+            status_code=400
+        )
